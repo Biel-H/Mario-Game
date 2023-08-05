9f061faf3b894cd096ac98a633ba45502fb2347e
Vai ter gameplay Com a Ashley? <gcamaro50@gmail.com>
2023-08-04 23:08:32 -0300

    Changes in js code

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const loop = setInterval(() => {
  
     
 
-    if (pipePosition <= 120&& pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
