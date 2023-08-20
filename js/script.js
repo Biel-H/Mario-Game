64fa6888e4fabf42259623311f44536f57d73171
Vai ter gameplay Com a Ashley? <gcamaro50@gmail.com>
2023-08-19 21:07:22 -0300

    updating interface by adding a 'Game-over' message. And adding a restart button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const over = document.querySelector('.overimage');
+const cloud = document.querySelector('.clouds')
+const restart = document.querySelector('.restart')
+ 
 
 const jump = () => {
     mario.classList.add('jump');
@@ -13,8 +17,6 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
  
-    
-
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
@@ -23,13 +25,22 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './images/game-over.png';
+        mario.src = './images/game-over.png'; 
         mario.style.width = '75px';
-        mario.style.marginLeft = '50px'
+        mario.style.marginLeft = '50px'; 
 
+        cloud.style.display = 'none';
+
+        restart.style.display = 'block';
+        restart.src = './images/restart.png';
+
+        over.style.display = 'block';
+        over.src = './images/game-over-image.png';
         clearInterval(loop);
+        //restart.addEventListener('click',() => {clearInterval(loop); restart.style.display='none'}) //função anonima
     }
-
 }, 10)
+
+//restart.addEventListener('click',location.replace(''))
 
 document.addEventListener('keydown', jump);
